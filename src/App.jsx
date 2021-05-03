03e829a13d719092aa27f50e44ffa04dbcd5f274
Jibril <jibrilf@lmtlss.dev>
2021-05-03 09:05:40 -0500

    Added section components for each resume section.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import "./App.css";
 import Section from "./components/Section.jsx";
 
 function App() {
+  // Editing index is a state for making sure only one section is being edited at a time.
   const [editingIndex, setEditingIndex] = useState(-1);
 
   return (
     <>
       <h1>Resume App</h1>
-      <Section sectionname="Section" editing={editingIndex == 0} onClose={() => setEditingIndex(-1)} onShow={() => setEditingIndex(0)} />
+      <Section sectionname="General Information" editing={editingIndex == 0} onClose={() => setEditingIndex(-1)} onShow={() => setEditingIndex(0)} />
+      <Section sectionname="Education" editing={editingIndex == 0} onClose={() => setEditingIndex(-1)} onShow={() => setEditingIndex(0)} />
+      <Section sectionname="Work Experience" editing={editingIndex == 0} onClose={() => setEditingIndex(-1)} onShow={() => setEditingIndex(0)} />
+      <Section sectionname="Skill" editing={editingIndex == 0} onClose={() => setEditingIndex(-1)} onShow={() => setEditingIndex(0)} />
     </>
   );
 }
